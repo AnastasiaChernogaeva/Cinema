a70f5da3725db50603f2decf97c11bc917378176
Nastya <sergeychernogaev1979@gmail.com>
2021-08-31 13:14:03 +0300

    LoST

--- testauthroles/src/main.js
+++ testauthroles/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmdclULRaun46ZcBR2J4VRAbPErlhsRsw",
+  authDomain: "testauthrole.firebaseapp.com",
+  projectId: "testauthrole",
+  storageBucket: "testauthrole.appspot.com",
+  messagingSenderId: "265934546726",
+  appId: "1:265934546726:web:ada75f7492e2a35b8e43e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
--- testauthroles/src/router/index.js
+++ testauthroles/src/router/index.js
@@ -1,25 +1,112 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue'
+import Router from 'vue-router'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+import firebase from 'firebase'
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import Admin from '@/views/Admin'
+import Driver from '@/views/Driver'
+import Customer from '@/views/Customer'
+
+// Vue.use(Router)
+
+let router = new Router({
+  routes: [{
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        guest: true
+      }
+    },
+
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: '/driver',
+      name: 'driver',
+      component: Driver,
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: '/customer',
+      name: 'customer',
+      component: Customer,
+      meta: {
+        auth: true
+      }
+    },
+  ],
 })
+
+
+
+
+
+
+router.beforeEach((to, from, next) => {
+
+  firebase.auth().onAuthStateChanged(userAuth => {
+
+    if (userAuth) {
+      firebase.auth().currentUser.getIdTokenResult()
+        .then(function ({
+          claims
+        }) {
+
+          if (claims.customer) {
+            if (to.path !== '/customer')
+              return next({
+                path: '/customer',
+              })
+          } else if (claims.admin) {
+            if (to.path !== '/admin')
+              return next({
+                path: '/admin',
+              })
+          } else if (claims.driver) {
+            if (to.path !== '/driver')
+              return next({
+                path: '/driver',
+              })
+          }
+
+        })
+    } else {
+      if (to.matched.some(record => record.meta.auth)) {
+        next({
+          path: '/login',
+          query: {
+            redirect: to.fullPath
+          }
+        })
+      } else {
+        next()
+      }
+    }
+
+  })
+
+  next()
+
+})
+
 
 export default router
