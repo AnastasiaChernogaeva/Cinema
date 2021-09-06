40579c4594c10db495ee0ce6fdc78ef4813a1b20
Nastya <sergeychernogaev1979@gmail.com>
2021-09-06 13:10:39 +0300

    save

--- main/src/tabs/Admin_Portal/hall/Row.vue
+++ main/src/tabs/Admin_Portal/hall/Row.vue
@@ -5,7 +5,7 @@
     <span class="indx" 
             :class="[
                  {'chosenSpan':infoArr && infoArr.find(info=>info.place===idx+1)},
-                 {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.find(placeOccupied=>placeOccupied.place===idx+1) },
+                 {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.includes(idx+1) },
                  ]"
             >
                 <span>{{idx+1}}</span>
@@ -13,18 +13,18 @@
           <img src="./icons/2x/simple.png" alt="simple hall places " class="hallPlaces simple" 
           :class="[
                 {'chosen':infoArr && infoArr.find(info=>info.place===idx+1)},
-                {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.find(placeOccupied=>placeOccupied.place===idx+1) }                ]" 
+                {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.includes(idx+1)  }                ]" 
                 v-if="simplePl &&simplePl.includes(rId)" >
           <img src="./icons/2x/vip.png" alt="vip hall places" class="hallPlaces vip" 
           :class="[
                 {'chosen':infoArr && infoArr.find(info=>info.place===idx+1)},
-                {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.find(placeOccupied=>placeOccupied.place===idx+1) },
+                {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.includes(idx+1)  },
                 ]" 
                 v-if="vipPl&&vipPl.includes(rId)" >
           <img src="./icons/2x/couple.png" alt="couple hall places" class="hallPlaces couple"
           :class="[
                 {'chosen':infoArr && infoArr.find(info=>info.place===idx+1)},
-                {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.find(placeOccupied=>placeOccupied.place===idx+1) },
+                {'isBooked':boughtT.find(boughtTInfo=>boughtTInfo.place===idx+1)||placesrowsBooked.includes(idx+1)  },
                 ]"
                 v-if="couplePl&&couplePl.includes(rId)" >
 </span>
@@ -73,10 +73,14 @@ export default {
         const boughtT = ref([])
 
 // ??????????????????????????????
-        if(props.occupiedPlaces!==undefined){
-       placesrowsBooked.value = props.occupiedPlaces.find(arr=>arr.filter(row=>row.row===props.rId))
+    if(props.occupiedPlaces!==undefined){
+       let row = props.occupiedPlaces.find(arr=>arr.filter(row=>row.row===props.rId))
+       row.forEach(element => {
+           placesrowsBooked.value.push(element.place)
+       });
+       
        console.log( placesrowsBooked.value);
-        }
+    }
 
 
 
