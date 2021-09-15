f9e16712856e444b85b43f643ae4158d349c9442
Nastya <sergeychernogaev1979@gmail.com>
2021-09-15 19:24:38 +0300

    src copy delete

--- main/src copy/tabs/Admin_Portal/use/currency.js
+++
@@ -1,6 +0,0 @@
-
-const formatterCurrency = new Intl.NumberFormat('ru-RU',{currency:'BYN', style:'currency',})
-
-export function currency(value){
-    return formatterCurrency.format(value)
-}
--- main/src copy/tabs/Admin_Portal/use/date.js
+++
@@ -1,6 +0,0 @@
-const formatterDate = new Intl.DateTimeFormat('ru-RU',{year: "numeric",
-month: "numeric",day: "numeric"})
-
-export function date(value){
-    return formatterDate.format(new Date(value.split('-').join(', ')))
-}
